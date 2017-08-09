df4bd87124525876b8e295511deccf3d84e2c550
yunz <mahe.paul93@gmail.com>
2017-08-09 22:56:58 +0800

    网易笔试题解

---
+++ Algorithm/src/interview/netease/AdjustQueue.java
@@ -0,0 +1,38 @@
+package interview.netease;
+
+import java.util.Scanner;
+
+/**
+ * 网易2017年春招题
+ * 调整队形
+ * 最终目标是将男孩移到最左边，或者将女孩移到最左边。
+ * 如果有B个男孩，则移到最左边的index分别为：0,1,2...B-1,所以所有index的和为（B-1）*B/2
+ * 一次遍历，计算目前男孩所在的index的和为sumB，则sumB减去上面的和就是所求的结果。
+ * 因此只要一次遍历，计算男孩所在的男孩的个数和男孩所在的index的和，求之差就行了。女孩同理。最后求最小值。
+ * Created by Administrator on 2017/8/9 0009.
+ */
+public class AdjustQueue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int bNum = 0;
+        int gNum = 0;
+
+        int bSum = 0;
+        int gSum = 0;
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == 'G'){
+                gSum+= i;
+                gNum++;
+            }else if(s.charAt(i) == 'B'){
+                bSum+= i;
+                bNum++;
+            }
+        }
+
+        int gRes = gSum - gNum*(gNum-1)/2;
+        int bRes = bSum - bNum*(bNum-1)/2;
+        System.out.println(Math.min(gRes, bRes));
+    }
+}
---
+++ Algorithm/src/interview/netease/TwoCoreDeal.java
@@ -0,0 +1,32 @@
+package interview.netease;
+
+/**
+ * 网易17年笔试题
+ * 双核处理
+ * 解题思路：01背包问题
+ * Created by Administrator on 2017/8/9 0009.
+ */
+import java.lang.*;
+import java.util.*;
+
+public class TwoCoreDeal {
+    public static void main(String[] args){
+        int num = 0;
+        Scanner scanner = new Scanner(System.in);
+        num = Integer.valueOf(scanner.nextLine());
+        String[] strings = scanner.nextLine().split(" ");
+        int[] task = new int[num];
+        int sum = 0;
+        for(int i = 0; i < num; i++){
+            task[i] = Integer.valueOf(strings[i]) / 1024;
+            sum += task[i];
+        }
+        int[][] handler = new int[num + 1][sum/2 + 1];
+        for(int i = 0; i <= num; i++)
+            for(int j = 0; j <= sum/2; j++){
+                handler[i][j] = i==0 ? 0 : handler[i - 1][j];
+                if(i > 0 && j >= task[i-1]) handler[i][j] = Math.max(handler[i-1][j], handler[i-1][j-task[i-1]] + task[i-1]);
+            }
+        System.out.print(Math.max(handler[num][sum/2], sum - handler[num][sum/2])*1024);
+    }
+}
