b084fb2e19a09f8fc51a8ab42db30542fa32f9fb
yunz <mahe.paul93@gmail.com>
2017-08-11 22:38:54 +0800

    网易笔试题解

---
+++ Algorithm/src/interview/netease/Aircraft.java
@@ -0,0 +1,22 @@
+package interview.netease;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * 网易17年笔试题
+ * 星际穿越
+ * Created by Administrator on 2017/8/11 0011.
+ */
+public class Aircraft {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()) {
+            long h = in.nextLong();
+            double res = Math.floor((Math.sqrt(1 + 4 * h) - 1) / 2);
+
+            DecimalFormat df = new DecimalFormat("0");
+            System.out.println(df.format(res));
+        }
+    }
+}
